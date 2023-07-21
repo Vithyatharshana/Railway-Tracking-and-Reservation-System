6e1825cf2de3d249e061e4353c3d34e4f71e5de5
Vithyatharshana N <76719711+Vithyatharshana@users.noreply.github.com>
2023-07-21 13:00:10 +0530

    Add files via upload

---
+++ Rail-Express-Services-main/Recruitment forms/coolie.php
@@ -0,0 +1,25 @@
+<?php
+	$Name = $_POST['Name'];
+	$email = $_POST['email'];
+	$Phone = $_POST['Phone'];
+    $Age= $_POST['Age'];
+	$Gender = $_POST['Gender'];
+	$Hrs = $_POST['Hrs'];
+	
+
+	$conn = new mysqli('localhost','root','','railway');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$sql = "insert into reccoolie(Name,email, Phone, Age, Gender,Hrs) values('$Name','$email', '$Phone', '$Age', '$Gender', '$Hrs');";
+		
+		$conn->query($sql);
+		$conn->close();
+		echo "<script>
+		window.location.href='Thank_You/index.html'
+	</script>";
+		
+		$conn->close();
+	}
+?>
---
+++ Rail-Express-Services-main/Recruitment forms/recman.php
@@ -0,0 +1,26 @@
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$phone = $_POST['phone'];
+    $age= $_POST['age'];
+	$gender = $_POST['gender'];
+	$qualifiation = $_POST['qualifiation'];
+	$salary = $_POST['salary'];
+	$message = $_POST['message'];
+
+	$conn = new mysqli('localhost','root','','railway');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$sql = "insert into recmedical(name,email,phone,age,gender,qualification,salary,message) values('$name','$email','$phone','$age','$gender','$qualification','$salary','$message');";
+		
+		$conn->query($sql);
+		$conn->close();
+		echo "<script>
+		window.location.href='Thank_You/index.html'
+	</script>";
+		
+		$conn->close();
+	}
+?>
